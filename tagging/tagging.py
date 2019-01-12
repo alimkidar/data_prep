51863a241fed9fcff58c4261d0633589813dae96
Alim Kidar Hanif <39610769+alimkidar@users.noreply.github.com>
2019-01-12 22:37:43 +0700

    Add files via upload

--- tagging/tagging.py
+++ tagging/tagging.py
@@ -1,70 +1,55 @@
 import pandas as pd
 import re
-
+from collections import Counter
 
 #------------------ CONFIGURATION -------------------------------
 dict_filename = "tagging_library.csv"
 input_filename = "convo.csv"
 
-
 #load convo
 convo = pd.read_csv(input_filename)
 
-
-#nama kolom di "dict_filename"
+# Fieldname in (dict_filename)
 head_keywords = 'keywords'
 head_category1 = 'category1'
 head_category2 = 'category2'
 head_category3 = 'category3'
-head_category_group = 'group_id'
 
-#nama baris di "input_filename"
-head_userid = 'user_id'
+# This script need this fieldname from input file (input_fieldname):
 head_username = 'username'
 head_caption = 'caption'
 head_postid = 'post_id'
-head_likes_count = 'like_count'
-head_comment_count = 'comment_count'
-head_brand = 'brand'
-head_date_post = 'date_post'
-
-
+head_timestamp = 'timestamp'
 
 #------------------ OUTPUT FILE -------------------------------
-#1. tb_convo_count_percent.csv
-#2. tb_pivot_percent.csv
-#3. tb_user_statistics.csv
-#4. tb_user_keywords.txt
-
+#1. Tagging_result.csv
+#2. Unlisted.csv
 
 #------------------	FUNCTION DEFINITIONS ------------------------
 def filter_words(sentence):
-	char = "!$%^&*()_-+=.?`~,ã:"
-
+	char = "$^&-+=.?`~,ã:"
 	for c in char:
 		sentence = sentence.replace(c,' ')
 	sentence = sentence.replace('"',' ')
 	sentence = sentence.replace("\n"," ")
-	sentence = sentence.replace("/"," ")
 	sentence = sentence.replace("'"," ")
 	sentence = sentence.replace("\\"," ")
 	sentence = sentence.replace("  "," ")
-	return sentence
+	sentence_lower = sentence.lower()
+	return sentence_lower
 
-#------------------ DATA LOADING --------------------------------
-
+#------------------ LOADING DATA ----------------------------
 print ("Memulai")
-
-#load dictionary dari file CSV yang bernama lib2.csv
+#load dictionary file (dict_filename)
 mydict = {}
 dfdict = pd.read_csv(dict_filename)
 list_category_group_match =  []
 list_category_group = {}
 for index, row in dfdict.iterrows():
-	keyword = str(row[head_keywords]).replace('"','').replace(",", "").replace("/"," ").replace("("," ").replace(")","").lower()
-	category1 = str(row[head_category1]).lower()
-	category2 = str(row[head_category2]).lower()
-	category3 = str(row[head_category3]).lower()
+	keyword = str(row[head_keywords]).lower()
+	category1 = str(row[head_category1])
+	category2 = str(row[head_category2])
+	category3 = str(row[head_category3])
 	
 	if len(category1) == 0:
 		category1 = 'not-available'	
@@ -72,8 +57,8 @@ for index, row in dfdict.iterrows():
 		category2 = 'not-available'
 	if len(category3) == 0:
 		category3 = 'not-available'
-
-	category_group = str(row[head_category_group])
+	#concating categories into a string
+	category_group = category1 + category2 + category3
 	if category_group not in list_category_group:
 		list_category_group[category_group] = []
 
@@ -86,71 +71,102 @@ list_category_group_match = list(set(list_category_group_match))
 
 #Seting Output6 ====================================================================
 output_name6 = "tagging_result.csv"
-f6 = open(output_name6, "w", encoding="utf-8")
-labelf6 = "Username,PostID,Conversation,Time Stamp,Keyword,ID_Group,Category1,Category2,Category3\n"
-f6.write(labelf6)
-
+f = open(output_name6, "w", encoding="utf-8")
+label = "username,post_id,caption,timestamp,keyword,category1,category2,category3\n"
+f.write(label)
 
 hit = 0
 hit_user = 1
 
-#Data merupakan sebuah kumpulan kata secara temporary pada setiap caption (BERSIFAT TEMPORARY)
+#temporary data for every matched keyword in every caption
 data = []
 
-#perhitungan tiap post
+#for counting posts
 post = []
 
-#mencatat ID Group di setiap convo
+#for listing  group_id
 post_id_group = {}
 all_post_id_group = []
 
-#perhitungan untuk semua post
+#for counting all posts
 user_post = {}
 
-#data user list
+#for listing username
 user_list = []
 
-# Mengolah Convo
+dict_unlisted_hashtag = []
+
+#Get data from Fieldname: username, caption, timestamp, post_id
 for index, row in convo.iterrows():
-	user_id = row[head_userid].replace('"', '').replace("'","")
-	username = row[head_username].replace('"',"").replace(",","|").replace("'","")
-	caption = filter_words(row[head_caption]).strip()
-	post_id = row[head_postid].replace('"','').replace("'","")
+	username = str(row[head_username]).replace(",","|")
+	caption_raw = row[head_caption]
+	caption_ = caption_raw.replace('#', ' #')
+	caption = filter_words(caption_).strip()
+	post_id = str(row[head_postid])
 	post_id_group[post_id] = []
-	brand = row[head_brand]
-	date_post = str(row[head_date_post])
+	timestamp = str(row[head_timestamp])
+	
+	#encode caption
+	if type(caption) != bytes:
+		caption = caption.encode('utf-8')
+	caption = str(caption).replace("b'", "").replace("'","")
+	# get only last 1000 char
+	if len(caption) > 30000:
+		x = len(caption) - 1000
+		caption = caption[x:]
 
-
-	hit += 1
+	#Parsing #(hashtag) from caption
+	token = caption.split()
+	token_hashtag = []
+	for word in token:
+		if '#' in word and len(word) > 1:
+			hashtag_temp = word.replace('#','')
+			token_hashtag.append(hashtag_temp)
+		else:
+			pass
+	#listing username into a list
 	if username not in user_list:
 		user_list.append(username)
 
-	#Untuk mencocokan keyword dalam caption
-	for i in mydict:
-		if i in caption:
-			post.append(mydict[i])
-			post_id_group[post_id].append(mydict[i]) #mydict[i] isinya IDGroup
-			all_post_id_group.append(mydict[i])
-			x_id_grup = mydict[i]
+	#Extracting hashtag which is not be found in library
+	for hashtag in token_hashtag:
+		hashtag = hashtag.replace('#','')
+		if hashtag not in mydict:
+			dict_unlisted_hashtag.append(hashtag)		
+	
+	#Searching keywords in every caption (conversation)
+	for keyword in mydict:
+		if keyword in caption:
+			post.append(mydict[keyword])
+			post_id_group[post_id].append(mydict[keyword]) #mydict[i] isinya IDGroup
+			all_post_id_group.append(mydict[keyword])
+			x_id_grup = mydict[keyword]
 
 			cat1_ = list_category_group[x_id_grup][0]
 			cat2_ = list_category_group[x_id_grup][1]
 			cat3_ = list_category_group[x_id_grup][2]
 
-			if type(caption) != bytes:
-				caption = caption.encode('utf-8')
-			caption = str(caption).replace("b'", "").replace("'","")
-			#menulis CSV tentang keyword yang cocok dengan convo/caption yang kena
+			#print into CSV file if keyword in caption
 			isi = str(username) + ', "' + str(post_id) + '",' + str(caption)
 			
-
-			#labelf6 = "Username,PostID,Conversation,Time Stamp,Likes,Comments,Engagement,Keyword,ID_Group,Category1,Category2,Category3\n"
+			#fieldname output = username | post_id | caption | timestamp | keyword | category1 | category2 | category3
 			id_group = str(x_id_grup)
 			linef6 = (username + ",'" + post_id + "," + caption + "," + 
-				date_post + "," + str(i) + "," + id_group + "," +  
+				timestamp + "," + str(keyword) + "," +  
 				cat1_ + "," + cat2_ + "," + cat3_ + "\n")
-			f6.write(linef6)
-	print("Convo ke-" + str(hit) + " berhasil diolah.")
-f6.close()
+			f.write(linef6)
+		else:
+			pass
+	hit += 1
+	print("Convo ke-" + str(hit) + " . Caption: " + caption)
+f.close()
+
+# Counting ublisted hashtag
+dict_unlisted_hashtag_count = Counter(dict_unlisted_hashtag)
+
+# Converting unlisted hashtag dataframe into csv file
+df_unlisted_hashtag = pd.DataFrame.from_dict(dict_unlisted_hashtag_count,orient='index').reset_index()
+df_unlisted_hashtag.columns = ['hashtag','count']
+df_unlisted_hashtag.to_csv('unlisted.csv')
 print("Data Tersimpan")
 print("Proses Selesai")
